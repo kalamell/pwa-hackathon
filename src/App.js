5b2c34be6b805362925b8e4cea6e16737b12a358
kalamell <sankhumpha84@gmail.com>
2017-06-24 18:44:53 +0700

    create people list && update css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import Nav from './Nav';
 import Footer from './Footer';
+
+import People from './People';
 class App extends Component {
   render() {
     return (
         <div className="container section">
             <Nav />
-
+            <People />
             <Footer />
         </div>
     )
