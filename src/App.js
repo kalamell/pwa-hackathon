8f8351796c6080ff14dcedea7aefee01c6a6edb4
kalamell <sankhumpha84@gmail.com>
2017-06-25 17:31:58 +0700

    signin and signup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import { firebaseApp } from './firebase';
 import 'bulma/css/bulma.css';
 import Create from './Create';
+import Signup from './Signup';
+import Signin from './Signin';
 import Nav from './Nav';
 import Footer from './Footer';
-
 import People from './People';
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false
+        }
+        firebaseApp.auth().onAuthStateChanged(user => {
+            if (user) {
+               this.setState({
+                loggedIn: true
+               })
+            }
+        });
+    }
   render() {
+
     return (
         <Router>
             <div className="container section">
                 <Nav />
                 <Route exact path="/" component={People}/>
-                <Route path="/create" component={Create}/>
+                <Route exact path="/create" render={() => (
+                  this.state.loggedIn ? (
+                    <Create />
+                  ) : (
+                    <Redirect to="/signin"/>
+                  )
+                )}/>
+
+                <Route path="/signin" component={Signin}/>
+                <Route path="/signup" component={Signup}/>
                 <Footer />
             </div>
         </Router>
     )
   }
 }
+/*
+<Route exact path="/" render={() => (
+  loggedIn ? (
+    <Redirect to="/dashboard"/>
+  ) : (
+    <PublicHomePage/>
+  )
+)}/>
+*/
 
 export default App;
