ccbe33731858976c263b85874233e12d917b771a
kalamell <sankhumpha84@gmail.com>
2017-06-25 19:44:53 +0700

    update crate form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+
 import { firebaseApp } from './firebase';
 import 'bulma/css/bulma.css';
 import Create from './Create';
@@ -8,6 +9,8 @@ import Signin from './Signin';
 import Nav from './Nav';
 import Footer from './Footer';
 import People from './People';
+
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -25,23 +28,25 @@ class App extends Component {
   render() {
 
     return (
-        <Router>
-            <div className="container section">
-                <Nav />
-                <Route exact path="/" component={People}/>
-                <Route exact path="/create" render={() => (
-                  this.state.loggedIn ? (
-                    <Create />
-                  ) : (
-                    <Redirect to="/signin"/>
-                  )
-                )}/>
 
-                <Route path="/signin" component={Signin}/>
-                <Route path="/signup" component={Signup}/>
-                <Footer />
-            </div>
-        </Router>
+            <Router>
+                <div className="container section">
+                    <Nav />
+                    <Route exact path="/" component={People}/>
+                    <Route exact path="/create" render={() => (
+                      this.state.loggedIn ? (
+                        <Create />
+                      ) : (
+                        <Redirect to="/signin"/>
+                      )
+                    )}/>
+
+                    <Route path="/signin" component={Signin}/>
+                    <Route path="/signup" component={Signup}/>
+                    <Footer />
+                </div>
+            </Router>
+
     )
   }
 }
--- src/Create.js
+++ src/Create.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
 import { firebaseApp } from './firebase';
-
+import {  Redirect, Link } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 class Create extends Component {
-
-    render() {
-        return (
-            <div>
-fdfdfdf
-            </div>
-        )
+    constructor(props) {
+        super(props);
+        firebaseApp.auth().onAuthStateChanged(user => {
+            if (!user) {
+               <Redirect to="/sigin" />
+            }
+        });
     }
+
+    handleSubmi() {
+
+    }
+    render() {
+            const style = {
+                'marginTop': '60px'
+            }
+            return (
+
+                <div style={style}>
+                <h1>แจ้งเบาะแส</h1>
+                 <div className="field">
+                   <label className="label">ข้อความ</label>
+                   <p className="control has-icons-left has-icons-right">
+                     <input className="input" type="text" placeholder="" />
+                     <span className="icon is-small is-left">
+                       <i className="fa fa-edit"></i>
+                     </span>
+                   </p>
+
+
+                 </div>
+                 <div className="field">
+                   <label className="label">ข้อความ</label>
+                   <p className="control">
+                     <textarea className="textarea" placeholder="ข้อความ"></textarea>
+                   </p>
+                 </div>
+
+                <button className="button is-primary" onClick={() => this.handleSubmi() }>แจ้งเบาะแส</button>
+                <p></p>
+                <br/>
+
+
+                </div>
+            )
+        }
 }
 
 export default Create;
